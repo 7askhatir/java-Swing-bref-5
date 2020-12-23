bc7b27d2680d671c7f2384e5a06ec4840a128a56
khalid <57253021+7askhatir@users.noreply.github.com>
2020-12-23 10:44:46 +0100

    Update Bref5.java

--- Bref5.java
+++ Bref5.java
@@ -23,8 +23,13 @@ public class Bref5 implements ActionListener {
                          "Est-ce que on peut avoir plusieurs constructeurs pour la même classe",
                          "BDans la ligne \"public class A implements B\", B est",
                          "Après la compilation, un programme écrit en\n" +
-                         "JAVA, il se transforme en programme en bytecode Quelle est l’extension du programme en bytecode ?",
-                         "Class Test",
+                         "JAVA, il se transforme en programme en bytecode Quelle )\n" +
+                         " est l’extension du programme en bytecode ?",
+                         "Class Test{ Public Test () { System.out.println(”Bonjour”);}\n" +
+                        "public Test (int i) { this();\n" +
+                        "System.out.println(”Nous sommes en ”+i+ ” !”);}; }\n" +
+                        "qu’affichera l’instruction suivante?\n" +
+                        "Test t1=new Test (2018);",
                          "Voici un constructeur de la classe Employee, y-at'il un problème Public void Employe(String n){ Nom=n;}",
                          "Pour spécifier que la variable ne pourra plus être\n" +
                          "modifiée et doit être initialisée dès sa déclaration,\n" +
@@ -36,7 +41,14 @@ public class Bref5 implements ActionListener {
                          "Est-ce qu’une classe peut implémenter plusieurs interfaces ?",
                          "La déclaration d'une méthode suivante :\n" +
                          "public void traitement() throws IOException précise que la méthode propage une exception contrôlée",
-                         "class Test",
+                         "class Test{\n" +
+                            "public static void main (String[] args) {\n" +
+                            "try {\n" +
+                            "int a =10; System.out.println (\"a = \" + a );\n" +
+                            "int b = 0 / a; System.out.println (\"b = \" + b); }\n" +
+                            "catch(ArithmeticException e)\n" +
+                            "{System.out.println (\"diviser par 0!\"); } finally\n" +
+                            "{System.out.println (\"je suis à l’intérieur de finally\");}}}",
                         
                         };
     String[][] Repons={{"Compilé","Interprété","Compilé et interpreté",""},
@@ -55,20 +67,20 @@ public class Bref5 implements ActionListener {
                        {"Vrai","Faux","",""},
                        {"aucun des choix","a=10 b=0 Je suis à l’intérieur de finally","a=10 b=0\n diviser par 0! je suis à l’intérieur de finally"," "},
     };
-    char[] Cas ={'3',
-                 '1',
+    char[] Cas ={'1',
+                 '2',
                  '3',
-                 '2',
-                 '1',
-                 '1',
-                 '1',
-                 '1',
                  '1',
                  '1',
                  '2',
                  '2',
                  '2',
+                 '1',
+                 '1',
                  '2',
+                 '2',
+                 '2',
+                 '1',
                  '2',
         
     };
@@ -95,7 +107,7 @@ public class Bref5 implements ActionListener {
     Bref5(int a){
         this.a=a;
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(650,650);
+        frame.setSize(680,770);
         frame.getContentPane().setBackground(new Color(200,200,200));
         
         textfield.setBounds(0, 0,650,50);
@@ -106,7 +118,7 @@ public class Bref5 implements ActionListener {
         /*textfield.setText("LV 1: : Question"+l.get(j).IQ);  */      
         textfield.setHorizontalAlignment(JTextField.CENTER);
         
-        textarea.setBounds(0, 50,650,50);
+        textarea.setBounds(0, 50,650,170);
         textarea.setBackground(new Color(25,25,25));
         textarea.setLineWrap(true);
         textarea.setEditable(false);
@@ -115,60 +127,60 @@ public class Bref5 implements ActionListener {
         /*textarea.setText("Q "+l.get(j).IQ+": "+l.get(j).Question); */
         textarea.setBorder(BorderFactory.createBevelBorder(1));
         
-        button1.setBounds(0, 100, 100, 100);
+        button1.setBounds(0, 220, 100, 100);
         button1.setFont(new Font("MV BOLI",Font.BOLD,35));
         button1.setFocusable(false);
         button1.addActionListener(this);
         button1.setText("1");
         
-        button2.setBounds(0, 200, 100, 100);
+        button2.setBounds(0, 320, 100, 100);
         button2.setFont(new Font("MV BOLI",Font.BOLD,35));
         button2.setFocusable(false);
         button2.addActionListener(this);
         button2.setText("2");
         
-        button3.setBounds(0, 300, 100, 100);
+        button3.setBounds(0, 420, 100, 100);
         button3.setFont(new Font("MV BOLI",Font.BOLD,35));
         button3.setFocusable(false);
         button3.addActionListener(this);
         button3.setText("3");
         
-        button4.setBounds(0, 400, 100, 100);
+        button4.setBounds(0, 520, 100, 100);
         button4.setFont(new Font("MV BOLI",Font.BOLD,35));
         button4.setFocusable(false);
         button4.addActionListener(this);
         button4.setText("4");
         
-        rep1.setBounds(125, 100, 500, 100);
+        rep1.setBounds(125, 220, 500, 100);
         rep1.setBackground(new Color(150,150,150));
         rep1.setFont(new Font("MV BOLI",Font.BOLD,25));
         rep1.setForeground(Color.BLACK);
         /*rep1.setText(l.get(j).Repons[0][1]);*/
         
-        rep2.setBounds(125, 200, 500, 100);
+        rep2.setBounds(125, 320, 500, 100);
         rep2.setBackground(new Color(150,150,150));
         rep2.setFont(new Font("MV BOLI",Font.BOLD,25));
         rep2.setForeground(Color.BLACK);
         /*rep2.setText(l.get(j).Repons[1][1]);*/
         
-        rep3.setBounds(125, 300, 500, 100);
+        rep3.setBounds(125, 420, 500, 100);
         rep3.setBackground(new Color(150,150,150));
         rep3.setFont(new Font("MV BOLI",Font.BOLD,25));
         rep3.setForeground(Color.BLACK);
         /*rep3.setText(l.get(j).Repons[2][1]);*/
         
-        rep4.setBounds(125, 400, 500, 100);
+        rep4.setBounds(125, 520, 500, 100);
         rep4.setBackground(new Color(150,150,150));
         rep4.setFont(new Font("MV BOLI",Font.BOLD,25));
         rep4.setForeground(Color.BLACK);
         /*rep4.setText(l.get(j).Repons[3][1]);*/
         
-        next.setBounds(535, 510, 100, 100);
+        next.setBounds(535, 630, 100, 100);
         next.setFont(new Font("MV BOLI",Font.BOLD,25));
         next.addActionListener(this);
         next.setText("Next");
         
-        next2.setBounds(0, 510, 100, 100);
+        next2.setBounds(0, 630, 100, 100);
         next2.setFont(new Font("MV BOLI",Font.BOLD,25));
         next2.addActionListener(this);
         
@@ -236,7 +248,7 @@ public class Bref5 implements ActionListener {
             case "4":if('4'==Cas[j+a])porsontage+=20;else porsontage+=0;j++;nextQ(a);break;  
             case "LV2" :   new  Bref5(5);break;
             case "LV3" :  new Bref5(10);break;
-            case "Fin" : JOptionPane.showMessageDialog(null, "Congratulation" , "InfoBox: ", JOptionPane.INFORMATION_MESSAGE);
+            case "Fin" : JOptionPane.showMessageDialog(null, "You wiiin" , "InfoBox: ", JOptionPane.INFORMATION_MESSAGE);
                               
                                                  
         }  
